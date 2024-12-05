2512995dff3ca7aee498dd9232cf5048c1546f63
swathieswaramoorthy <126444648+swathieswaramoorthy@users.noreply.github.com>
2024-12-05 21:39:18 +0530

    altr4

--- main/script.js
+++ main/script.js
@@ -3,11 +3,9 @@ let display = document.getElementById("display");
 function clearDisplay() {
   display.value = "";
 }
-
 function deleteLast() {
   display.value = display.value.slice(0, -1);
 }
-
 function appendNumber(number) {
   display.value += number;
 }
